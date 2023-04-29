eb29479633c2d79f5ef8327a26c44cbdbc46490d
Rob Rossmiller <rrossmiller3@gmail.com>
2023-04-29 01:41:17 -0400

    smart numbering

--- lua/RKR/init.lua
+++ lua/RKR/init.lua
@@ -10,3 +10,30 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+
+
+
+--  trying custom version of sitiom/nvim-numbertoggle
+local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+   pattern = "*",
+   group = augroup,
+   callback = function()
+      if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+         vim.opt.relativenumber = true
+      end
+   end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+   pattern = "*",
+   group = augroup,
+   callback = function()
+      if vim.o.nu then
+         vim.opt.relativenumber = false
+         vim.cmd "redraw"
+      end
+   end,
+})
