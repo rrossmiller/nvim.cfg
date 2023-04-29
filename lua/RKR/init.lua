8d98ed0f41440df1bce6e2af2d0361343737353f
Rob Rossmiller <rrossmiller3@gmail.com>
2023-04-29 01:51:32 -0400

    saveg

--- lua/RKR/init.lua
+++ lua/RKR/init.lua
@@ -4,11 +4,11 @@ require("RKR.set")
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
 })
 
 
@@ -18,22 +18,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
 
 vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
-   pattern = "*",
-   group = augroup,
-   callback = function()
-      if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
-         vim.opt.relativenumber = true
-      end
-   end,
+    pattern = "*",
+    group = augroup,
+    callback = function()
+        if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+            vim.opt.relativenumber = true
+        end
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
-   pattern = "*",
-   group = augroup,
-   callback = function()
-      if vim.o.nu then
-         vim.opt.relativenumber = false
-         vim.cmd "redraw"
-      end
-   end,
+    pattern = "*",
+    group = augroup,
+    callback = function()
+        if vim.o.nu then
+            vim.opt.relativenumber = false
+            vim.cmd "redraw"
+        end
+    end,
 })
