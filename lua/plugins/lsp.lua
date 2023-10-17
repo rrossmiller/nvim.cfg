aac8f33451b38f4c6fa2319abaff2167d6693b50
Rossmiller, Robert <rrossmiller3@gmail.com>
2023-10-17 11:14:35 -0400

    lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -117,7 +117,17 @@ return {
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
+      {
+        'j-hui/fidget.nvim',
+        enabled = false,
+        tag = 'legacy',
+        opts = {
+          timer = {
+            fidget_decay = 1000
+          },
+
+        }
+      },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
