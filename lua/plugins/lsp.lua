aa6bbf179f396a45cf374f7a821d18f1a6c16b10
Rossmiller, Robert <rrossmiller3@gmail.com>
2024-01-29 10:52:32 -0500

    prettier

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -62,7 +62,6 @@ local function cfg()
       --   nmap("<leader>f",
       --     function()
       --       vim.cmd("!jq .")
-      --       -- end, "Format current buffer with Black and isort")
       --     end, "Format current buffer with jq")
     elseif ft == "sh" then
       -- local black = string.format("silent !black --quiet %s", pth)
@@ -73,7 +72,7 @@ local function cfg()
           -- end, "Format current buffer with Black and isort")
         end, "Format current buffer with shfmt")
     elseif ft == "typescriptreact" or ft == 'javascriptreact' or ft == 'javascript' or ft == 'typescript' then
-      local prettier = string.format("silent !prettier -w %s", pth)
+      local prettier = string.format("silent !prettier --tab-width 2 -w %s", pth)
       nmap("<leader>f",
         function()
           vim.cmd(prettier)
@@ -172,23 +171,23 @@ return {
     event = "VeryLazy",
     opts = {
 
-      debug = false,                                          -- set to true to enable debug logging
+      debug = false,                                              -- set to true to enable debug logging
       log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
       -- default is  ~/.cache/nvim/lsp_signature.log
-      verbose = false,                                        -- show debug line number
+      verbose = false,                                            -- show debug line number
 
-      bind = true,                                            -- This is mandatory, otherwise border config won't get registered.
+      bind = true,                                                -- This is mandatory, otherwise border config won't get registered.
       -- If you want to hook lspsaga or other signature handler, pls set to false
-      doc_lines = 10,                                         -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+      doc_lines = 10,                                             -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
       -- set to 0 if you DO NOT want any API comments be shown
       -- This setting only take effect in insert mode, it does not affect signature help in normal
       -- mode, 10 by default
 
-      max_height = 12,                   -- max height of signature floating_window
-      max_width = 80,                    -- max_width of signature floating_window, line will be wrapped if exceed max_width
+      max_height = 12,                       -- max height of signature floating_window
+      max_width = 80,                        -- max_width of signature floating_window, line will be wrapped if exceed max_width
       -- the value need >= 40
-      wrap = true,                       -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
-      floating_window = false,            -- show hint in a floating window, set to false for virtual text only mode
+      wrap = true,                           -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
+      floating_window = false,               -- show hint in a floating window, set to false for virtual text only mode
 
       floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
       -- will set to true when fully tested, set to false will use whichever side has more space
@@ -209,28 +208,28 @@ return {
       -- return 'right_align' to display hint right aligned in the current line
       hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
       handler_opts = {
-        border = "rounded"                      -- double, rounded, single, shadow, none, or a table of borders
+        border = "rounded"                          -- double, rounded, single, shadow, none, or a table of borders
       },
 
-      always_trigger = false,               -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
+      always_trigger = false,                   -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
 
-      auto_close_after = nil,               -- autoclose signature float win after x sec, disabled if nil.
-      extra_trigger_chars = {},             -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
-      zindex = 200,                         -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
+      auto_close_after = nil,                   -- autoclose signature float win after x sec, disabled if nil.
+      extra_trigger_chars = {},                 -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
+      zindex = 200,                             -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
 
-      padding = '',                         -- character to pad on left and right of signature can be ' ', or '|'  etc
+      padding = '',                             -- character to pad on left and right of signature can be ' ', or '|'  etc
 
-      transparency = nil,                   -- disabled by default, allow floating win transparent value 1~100
-      shadow_blend = 36,                    -- if you using shadow as border use this set the opacity
-      shadow_guibg = 'Black',               -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
-      timer_interval = 200,                 -- default timer check interval set to lower value if you want to reduce latency
-      toggle_key = nil,                     -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+      transparency = nil,                       -- disabled by default, allow floating win transparent value 1~100
+      shadow_blend = 36,                        -- if you using shadow as border use this set the opacity
+      shadow_guibg = 'Black',                   -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
+      timer_interval = 200,                     -- default timer check interval set to lower value if you want to reduce latency
+      toggle_key = nil,                         -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
       toggle_key_flip_floatwin_setting = false, -- true: toggle floating_windows: true|false setting after toggle key pressed
       -- false: floating_windows setup will not change, toggle_key will pop up signature helper, but signature
       -- may not popup when typing depends on floating_window setting
 
       select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
-      move_cursor_key = nil,  -- imap, use nvim_set_current_win to move cursor between current win
+      move_cursor_key = nil,      -- imap, use nvim_set_current_win to move cursor between current win
     },
     -- config = function(_, opts)
     --   require 'lsp_signature'.setup(opts)
