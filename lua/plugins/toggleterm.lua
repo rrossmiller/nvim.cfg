a473926aeaee39d97771dff732e1d4b4068284b1
Rossmiller, Robert <rrossmiller3@gmail.com>
2024-02-13 10:17:59 -0500

    init term on startup

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -7,5 +7,22 @@ return {
         float_opts = {
             border = 'single' -- 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
         }
-    }
+    },
+
+    config = function()
+        require("toggleterm").setup({
+            open_mapping = [[Ô]],
+            direction = 'float',  -- 'vertical' | 'horizontal' | 'tab' | 'float',
+            float_opts = {
+                border = 'single' -- 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+            }
+        }
+        )
+        vim.api.nvim_create_autocmd({ "VimEnter" }, {
+            callback = function()
+                vim.cmd("ToggleTerm")
+                vim.cmd("ToggleTerm")
+            end,
+        })
+    end
 }
