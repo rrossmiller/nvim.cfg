c13226bab0f6cdd5f285551dfc042d92a5b3d877
Rob Rossmiller <rrossmiller3@gmail.com>
2024-06-16 23:14:54 -0400

    update

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,51 +1,51 @@
 return { -- Autoformat
-    "stevearc/conform.nvim",
-    lazy = false,
-    event = 'LspAttach',
-    keys = {
-        {
-            "<leader>f",
-            function()
-                require("conform").format { async = true, lsp_fallback = true }
-            end,
-            mode = "",
-            desc = "[F]ormat buffer",
-        },
-        {
-            "Ï",
-            function()
-                require("conform").format { async = true, lsp_fallback = true }
-            end,
-            mode = "",
-            desc = "Format buffer",
-        },
+  "stevearc/conform.nvim",
+  lazy = false,
+  event = "LspAttach",
+  keys = {
+    {
+      "<leader>f",
+      function()
+        require("conform").format { async = true, lsp_fallback = true }
+      end,
+      mode = "",
+      desc = "[F]ormat buffer",
     },
-    opts = {
-        notify_on_error = false,
-        -- format_on_save = function(bufnr)
-        --     -- Disable "format_on_save lsp_fallback" for languages that don't
-        --     -- have a well standardized coding style. You can add additional
-        --     -- languages here or re-enable it for the disabled ones.
-        --     local disable_filetypes = { c = true, cpp = true }
-        --     return {
-        --         timeout_ms = 500,
-        --         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        --     }
-        -- end,
-        formatters_by_ft = {
-            lua = { "stylua" },
-            -- Conform can also run multiple formatters sequentially
-            python = { "isort", "ruff_format" },
-            go = { "gofmt" },
-            sh = { "shfmt" },
-            -- sub-list torun *until* a formatter is found.
-            javascript = { { "prettierd", "prettier" } },
-        },
-        -- Customize formatters
-        -- formatters = {
-        --     shfmt = {
-        --         prepend_args = { "-i", "2" },
-        --     },
-        -- },
+    {
+      "Ï",
+      function()
+        require("conform").format { async = true, lsp_fallback = true }
+      end,
+      mode = "",
+      desc = "Format buffer",
     },
+  },
+  opts = {
+    notify_on_error = false,
+    -- format_on_save = function(bufnr)
+    --     -- Disable "format_on_save lsp_fallback" for languages that don't
+    --     -- have a well standardized coding style. You can add additional
+    --     -- languages here or re-enable it for the disabled ones.
+    --     local disable_filetypes = { c = true, cpp = true }
+    --     return {
+    --         timeout_ms = 500,
+    --         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+    --     }
+    -- end,
+    formatters_by_ft = {
+      lua = { "stylua" },
+      -- Conform can also run multiple formatters sequentially
+      python = { "isort", "ruff_format" },
+      go = { "gofmt" },
+      sh = { "shfmt" },
+      -- sub-list torun *until* a formatter is found.
+      javascript = { { "prettierd", "prettier" } },
+    },
+    -- Customize formatters
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2" },
+      },
+    },
+  },
 }
