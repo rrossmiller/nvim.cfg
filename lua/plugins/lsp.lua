559a0e1b1d4c082bfa84e2780ada3221a6bc916b
Rob Rossmiller <rrossmiller3@gmail.com>
2024-11-08 10:20:50 -0500

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -201,6 +201,28 @@ return {
           end,
         },
       }
+
+      require("lspconfig").svelte.setup {
+        filetypes = { "svelte" },
+        on_attach = function(client, bufnr)
+          if client.name == "svelte" then
+            vim.api.nvim_create_autocmd("BufWritePost", {
+              pattern = { "*.js", "*.ts", "*.svelte" },
+              callback = function(ctx)
+                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
+              end,
+            })
+          end
+          if vim.bo[bufnr].filetype == "svelte" then
+            vim.api.nvim_create_autocmd("BufWritePost", {
+              pattern = { "*.js", "*.ts", "*.svelte" },
+              callback = function(ctx)
+                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
+              end,
+            })
+          end
+        end,
+      }
     end,
   },
 }
