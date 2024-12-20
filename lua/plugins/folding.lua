c7ca9cf55b38924be1f2507a6d3353632291082f
Rob Rossmiller <rrossmiller3@gmail.com>
2024-12-20 18:12:24 -0500

    folding

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -2,6 +2,8 @@ return {
   "kevinhwang91/nvim-ufo",
   dependencies = "kevinhwang91/promise-async",
   config = function()
+    -- zc to fold
+    -- zo to open fold
     vim.o.foldcolumn = "0" -- '0' is not bad
     vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
