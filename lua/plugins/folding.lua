63fb599d77a0d178d8ee91577f4adce833df48ad
Rossmiller <robert_rossmiller+uhg@optum.com>
2025-02-14 09:33:54 -0500

    folding plugin changed how to get lsp client

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -21,7 +21,7 @@ return {
       dynamicRegistration = false,
       lineFoldingOnly = true,
     }
-    local language_servers = lsp.util._available_servers() -- or list servers manually like {'gopls', 'clangd'}
+    local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
     for _, ls in ipairs(language_servers) do
       lsp[ls].setup {
         capabilities = capabilities,
