6c58ffc62882717fdaf5008143590be21df435a4
rrossmil_uhg <robert_rossmiller+uhg@optum.com>
2025-09-11 21:36:14 -0400

    save

--- lsp/spellcheck.lua
+++ lsp/spellcheck.lua
@@ -1,42 +1,7 @@
-local home = os.getenv "HOME"
-local user = os.getenv "USER"
 local bin_path = vim.fn.stdpath "state"
--- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
--- if user == "robrossmiller" then
--- add filetype info
--- vim.filetype.add {
--- pattern = {
--- [home .. "/.config/ghostty/.*"] = "",
--- },
--- }
-
--- attach lsp
-
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = { "text", "marksdown" },
---   callback = function()
---     local client = vim.lsp.start {
---       name = "ghostty-lsp",
---       cmd = { bin_path .. "/spellcheck-lsp/lsp" },
---     }
---     if not client then
---       vim.notify "The spellcheck lsp client is not set up correctly"
---     else
---       vim.lsp.buf_attach_client(0, client)
---     end
---   end,
--- })
---
--- format lsp logs
--- vim.keymap.set("n", "<leader>rr", [[:%s/\\n/\r\t/g<CR><cmd>nohlsearch<CR>]])
--- end
 
 return {
   cmd = { bin_path .. "/spellcheck-lsp/lsp" },
-  filetypes = { "text", "markdown" },
+  filetypes = { "text", "markdown", "typst" },
   settings = {},
-  on_exit = function(code, _, _)
-    vim.notify("Closing Spellcheck LSP exited with code: " .. code, vim.log.levels.INFO)
-  end,
 }
