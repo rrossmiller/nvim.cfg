57eca02061a37d07d7de7e911a53af1cd0001923
Rob Rossmiller <rrossmiller3@gmail.com>
2025-12-20 00:10:47 -0500

    fix ts breaking changes

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -27,14 +27,44 @@ return {
         lualine_c = { { "filename", path = 1 } },
         -- lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_x = { "fileformat", "filetype" },
-        lualine_y = { "location" },
-        lualine_z = { "" },
+        lualine_y = {
+          "location",
+          {
+            "searchcount",
+            maxcount = 999,
+            timeout = 500,
+          },
+          -- {
+          --   "lsp_status",
+          --   icon = "", -- f013
+          --   symbols = {
+          --     -- Standard unicode symbols to cycle through for LSP progress:
+          --     spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+          --     -- Standard unicode symbol for when LSP is done:
+          --     done = "✓",
+          --     -- Delimiter inserted between LSP names:
+          --     separator = "│",
+          --   },
+          --   -- List of LSP names to ignore (e.g., `null-ls`):
+          --   ignore_lsp = { "GitHub Copilot" },
+          --   -- Display the LSP name
+          --   show_name = true,
+          -- },
+        },
+        lualine_z = {},
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = {
+          "location",
+          {
+            "searchcount",
+            maxcount = 999,
+            timeout = 500,
+          },
+        },
         lualine_y = {},
         lualine_z = {},
       },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,56 +1,34 @@
+local ensure_installed = {
+  "go",
+  "lua",
+  "python",
+  "rust",
+  -- "tsx",
+  "javascript",
+  "typescript",
+  "vimdoc",
+  "vim",
+}
 return {
-  { -- Highlight, edit, and navigate code
+  {
+    -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    lazy = false,
     dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
+      -- "nvim-treesitter/nvim-treesitter-context",
+      -- "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    opts = {
-      ensure_installed = {
-        "go",
-        "lua",
-        "python",
-        "rust",
-        -- "tsx",
-        "javascript",
-        "typescript",
-        "vimdoc",
-        "vim",
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { "ruby" },
-        disable = function(lang, buf)
-          local max_file_size = 100 * 1024
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_file_size then
-            return true
-          end
-        end,
-      },
-      indent = { enable = true, disable = { "ruby" } },
-    },
-    config = function(_, opts)
 
-
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-      -- Prefer git instead of curl in order to improve connectivity in some environments
-      require("nvim-treesitter.install").prefer_git = true
-
-      ---@diagnostic disable-next-line: missing-fields
-      require("nvim-treesitter.configs").setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    -- There are additional nvim-treesitter modules that you can use to interact
+    -- with nvim-treesitter. You should go explore a few and see what interests you:
+    --
+    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    config = function()
+      local ts = require "nvim-treesitter"
+      ts.install(ensure_installed)
     end,
   },
   {
@@ -59,4 +37,5 @@ return {
       { "<leader>tP", "<cmd>TSPlaygroundToggle<cr>", desc = "TS toggle playground" },
     },
   },
+  -- { "nvim-treesitter/nvim-treesitter-textobjects" },
 }
